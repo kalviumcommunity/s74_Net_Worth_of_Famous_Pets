48c3f55c4947b818f5e177e9b56d8d7812e6b580
Dhayanithi <545nithi@gmail.com>
2025-08-13 11:12:49 +0530

    cleared-error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Login from "./pages/login.jsx";
+import Login from "./pages/Login.jsx";
 import MyPage from "./pages/MyPage.jsx";
 import Signup from "./pages/Signup.jsx";
 import CreatePet from "./components/CreatePet.jsx";
